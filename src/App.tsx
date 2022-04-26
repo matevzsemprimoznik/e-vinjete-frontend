3ad1369ea4611c432839dcdb78c603a97d4db891
Matevz <semprimoznik.matevz@gmail.com>
2022-04-26 19:52:21 +0200

    add feature to edit vignettes

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import AddPurchase from './pages/AddPurchase';
 import VignetteValidation from './pages/VignetteValidation';
 import VignetteDetails from './pages/VignetteDetails';
 import PurchaseDetails from './pages/PurchaseDetails';
+import SaveVignette from './pages/SaveVignette';
 
 function App() {
   return (
@@ -24,7 +25,10 @@ function App() {
           <Route path={Paths.HOME} element={<Home />} />
           <Route path={Paths.ALL_VIGNETTES} element={<AllVignettes />} />
           <Route path={Paths.VIGNETTE + '/:id'} element={<VignetteDetails />} />
-          <Route path={Paths.SAVE_VIGNETTE} element={<AddVignette />} />
+          <Route path={Paths.SAVE_VIGNETTE}>
+            <Route path=':id' element={<SaveVignette />} />
+            <Route path='' element={<SaveVignette />} />
+          </Route>
           <Route path={Paths.PURCHASE + '/:id'} element={<PurchaseDetails />} />
           <Route path={Paths.ALL_PURCHASES} element={<AllPurchases />} />
           <Route path={Paths.ADD_PURCHASE} element={<AddPurchase />} />
